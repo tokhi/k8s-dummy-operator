a8ae4c9379027b28c6544d80e83898c6af7b0a16
Shafi Tokhi <shafi.tokhi@aroundhome.de>
2023-05-30 23:41:34 +0200

    dummy types

--- api/v1alpha1/dummy_types.go
+++ api/v1alpha1/dummy_types.go
@@ -1,45 +1,20 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
-import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 // DummySpec defines the desired state of Dummy
 type DummySpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Dummy. Edit dummy_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Message string `json:"message,omitempty"`
 }
 
 // DummyStatus defines the observed state of Dummy
 type DummyStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	SpecEcho  string `json:"specEcho,omitempty"`
+	PodStatus string `json:"podStatus,omitempty"`
 }
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 
 // Dummy is the Schema for the dummies API
 type Dummy struct {
@@ -50,7 +25,7 @@ type Dummy struct {
 	Status DummyStatus `json:"status,omitempty"`
 }
 
-//+kubebuilder:object:root=true
+// +kubebuilder:object:root=true
 
 // DummyList contains a list of Dummy
 type DummyList struct {
